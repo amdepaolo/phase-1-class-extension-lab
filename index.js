5543fafac7125a7ee56993cf90a58540fcd0554e
Adam DePaolo <amdepaolo@gmail.com>
2022-05-29 16:33:40 -0400

    commit tree ddcb5b2e858cdf3200947468766baefea442565e

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Polygon {
+    constructor(array){
+        this.sides = array;
+    }
+    get countSides(){
+        return this.sides.length;
+    }
+    get perimeter(){
+        let result = 0;
+        for (const side of this.sides) {
+         result += side;   
+        }
+        return result;
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        for (const side of this.sides){
+            if(this.perimeter - side < side) return false
+        }
+        return true
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        for( const side of this.sides){
+            if( side !== (this.perimeter/4)){
+                return false
+            }
+        }
+        return true
+    }
+    get area(){
+        return this.sides[0] ** 2
+    }
+}
